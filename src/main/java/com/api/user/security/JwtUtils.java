e22d71031a2a8d3afb0dc4d1fa35e2b6123a6230
salomonPr <nkurunzizasalomonbirori@gmail.com>
2025-11-19 10:57:51 +0200

    fix some bugs

--- src/main/java/com/api/user/security/JwtUtils.java
+++ src/main/java/com/api/user/security/JwtUtils.java
@@ -1,95 +1,95 @@
-//package com.api.user.security;
-//
-//import com.api.user.entity.Role;
-//import io.jsonwebtoken.Claims;
-//import io.jsonwebtoken.JwtException;
-//import io.jsonwebtoken.Jwts;
-//import io.jsonwebtoken.security.Keys;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.stereotype.Component;
-//
-//import javax.crypto.SecretKey;
-//import java.nio.charset.StandardCharsets;
-//import java.util.Date;
-//import java.util.HashMap;
-//import java.util.Map;
-//
-//@Component
-//public class JwtUtils {
-//
-//    // secret key loaded from application.properties
-//    private final SecretKey SECRET_KEY;
-//
-//    // token valid for 24 hours
-//    private final long EXPIRATION_TIME = 24 * 60 * 60 * 1000;
-//
-//    public JwtUtils(@Value("${jwt.secret}") String secret) {
-//        this.SECRET_KEY = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
-//    }
-//
-//
-//
-//    public String generateToken(String username, Role role) {
-//        Map<String, Object> claims = new HashMap<>();
-//        claims.put("role", role.name());
-//
-//        return Jwts.builder()
-//                .claims(claims)
-//                .subject(username)
-//                .issuedAt(new Date())
-//                .expiration(new Date(System.currentTimeMillis()+EXPIRATION_TIME))
-//                .signWith(SECRET_KEY, Jwts.SIG.HS256)
-//                .compact();
-//    }
-//
-//    // here we extract username from token
-//    public String getUsernameFromToken(String token) {
-//        try {
-//            return extractAllClaims(token).getSubject();
-//        } catch (JwtException | IllegalArgumentException e) {
-//            throw new RuntimeException("Invalid JWT token", e);
-//        }
-//    }
-//
-//    // extract role from token
-//    public Role getRoleFromToken(String token) {
-//        try {
-//            String roleStr = (String) extractAllClaims(token).get("role");
-//            if (roleStr == null) {
-//                throw new RuntimeException("Role not found in JWT token");
-//            }
-//            return Role.valueOf(roleStr);
-//        } catch (JwtException | IllegalArgumentException e) {
-//            throw new RuntimeException("Invalid JWT token or role", e);
-//        }
-//    }
-//
-//    // check if the token is expired
-//    public boolean isTokenExpired(String token) {
-//        try {
-//            return extractAllClaims(token).getExpiration().before(new Date());
-//        } catch (JwtException | IllegalArgumentException e) {
-//            return true;
-//        }
-//    }
-//
-//
-//    // check token validation
-//    public boolean validateToken(String token, String username) {
-//        try {
-//            String tokenUsername = getUsernameFromToken(token);
-//            return (tokenUsername.equals(username) && !isTokenExpired(token));
-//        } catch (Exception e) {
-//            return false;
-//        }
-//    }
-//
-//    private Claims extractAllClaims(String token) {
-//        return Jwts.parser()
-//                .verifyWith(SECRET_KEY)
-//                .build()
-//                .parseSignedClaims(token)
-//                .getPayload();
-//    }
-//
-//}
+package com.api.user.security;
+
+import com.api.user.entity.Role;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtUtils {
+
+    // secret key loaded from application.properties
+    private final SecretKey SECRET_KEY;
+
+    // token valid for 24 hours
+    private final long EXPIRATION_TIME = 24 * 60 * 60 * 1000;
+
+    public JwtUtils(@Value("${jwt.secret}") String secret) {
+        this.SECRET_KEY = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
+
+
+    public String generateToken(String username, Role role) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("role", role.name());
+
+        return Jwts.builder()
+                .claims(claims)
+                .subject(username)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis()+EXPIRATION_TIME))
+                .signWith(SECRET_KEY, Jwts.SIG.HS256)
+                .compact();
+    }
+
+    // here we extract username from token
+    public String getUsernameFromToken(String token) {
+        try {
+            return extractAllClaims(token).getSubject();
+        } catch (JwtException | IllegalArgumentException e) {
+            throw new RuntimeException("Invalid JWT token", e);
+        }
+    }
+
+    // extract role from token
+    public Role getRoleFromToken(String token) {
+        try {
+            String roleStr = (String) extractAllClaims(token).get("role");
+            if (roleStr == null) {
+                throw new RuntimeException("Role not found in JWT token");
+            }
+            return Role.valueOf(roleStr);
+        } catch (JwtException | IllegalArgumentException e) {
+            throw new RuntimeException("Invalid JWT token or role", e);
+        }
+    }
+
+    // check if the token is expired
+    public boolean isTokenExpired(String token) {
+        try {
+            return extractAllClaims(token).getExpiration().before(new Date());
+        } catch (JwtException | IllegalArgumentException e) {
+            return true;
+        }
+    }
+
+
+    // check token validation
+    public boolean validateToken(String token, String username) {
+        try {
+            String tokenUsername = getUsernameFromToken(token);
+            return (tokenUsername.equals(username) && !isTokenExpired(token));
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(SECRET_KEY)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+}
